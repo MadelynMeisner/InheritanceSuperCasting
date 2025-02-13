84d3cba4bcf01aaacfaa1515c0609ba3627c734d
madelyn.meisner <madelyn.meisner@MU-C1-12.mullen.pvt.k12.co.us>
2025-02-13 13:13:06 -0700

    poop

---
+++ InheritanceSuperCasting/src/InheritanceSuperCasting.java
@@ -0,0 +1,18 @@
+
+public class InheritanceSuperCasting
+	{
+
+		public static void main(String[] args)
+			{
+				Men x = new Men();
+				System.out.println(x.isSuperHot);
+				Men x = new Men();
+				System.out.println(x.isSuperHot);
+				Men x = new Men();
+				System.out.println(x.isSuperHot);
+
+			}
+
+	}
+		
+		
