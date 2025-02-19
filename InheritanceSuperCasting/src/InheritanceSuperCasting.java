ff95765d159059260a6dade047afd9a9f0c6c415
madelyn.meisner <madelyn.meisner@MU-C1-12.mullen.pvt.k12.co.us>
2025-02-19 13:16:31 -0700

    recnt recent

--- InheritanceSuperCasting/src/InheritanceSuperCasting.java
+++ InheritanceSuperCasting/src/InheritanceSuperCasting.java
@@ -2,10 +2,20 @@ public class InheritanceSuperCasting
 {
     public static void main(String[] args) 
     {
+        Men x = new  Men("poop");
+        x.canBreathe();
+        Men x = new  Men("poop");
+        x.makesFunnyJokes();
         Men x = new MyBoyfriend("poop");
-
-        x.canBreathe(); 
-        x.makesFunnyJokes(); 
+        x.canBreathe();
+        Men x = new MyBoyfriend("poop");
+        x.makesFunnyJokes();
+        MyBoyfriend x = new MyBoyfriend("poop");
+        x.canBreathe();
+        MyBoyfriend x = new MyBoyfriend("poop");
+        x.makesFunnyJokes();
+        
+     
 
     
     }
--- InheritanceSuperCasting/src/Men.java
+++ InheritanceSuperCasting/src/Men.java
@@ -1,4 +1,4 @@
-public class Men 
+public abstract class Men 
 {
     public Men() 
     {
@@ -14,6 +14,9 @@ public class Men
     {
         System.out.println("*gasp*");
     }
+    
+    public abstract void eatsHealthy();
+  
 
     public void makesFunnyJokes() 
     {
